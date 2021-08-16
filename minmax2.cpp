d9453c537914bc82762796489f6dd52bb3346f0f
fair1478 <pattaradanai4290@gmail.com>
2021-08-16 18:34:38 +0700

    B

---
+++ minmax2.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int P[1000];
+main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+        scanf("%d",&P[i]);
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(P[j]>P[i])
+            {
+                int l=P[j];
+                P[j]=P[i];
+                P[i]=l;
+            }
+        }
+    }
+
+    
+        printf("%d\n%d",P[n-1],P[0]);
+
+}
